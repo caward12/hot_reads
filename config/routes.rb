f2a9c34377a2835e4965e629d3aad376567f37b4
caward12 <ward.colleen.a@gmail.com>
2017-07-27 12:04:52 -0600

    add links index endpoint to return hot links

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
-      resources :links, only: [:create]
+      resources :links, only: [:index, :create]
     end
   end
 
